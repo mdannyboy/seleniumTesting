3c1ab3173a4b13f9432a5ffaaa5447bad2684f43
mdannyboy <michael.dannyboy@gmail.com>
2023-10-15 20:49:10 -0500

    This works. Opens site, clicks the "download" button, and ends.

--- seleniumentry.py
+++ seleniumentry.py
@@ -1,6 +1,8 @@
 #print("hello")
 
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # driver = webdriver.Chrome()
 # driver.get('https://www.google.com/')
@@ -10,6 +12,8 @@ driver = webdriver.Chrome()
 # driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
 driver.get("https://demo.seleniumeasy.com/jquery-download-progress-bar-demo.html")
 driver.implicitly_wait(30)
-my_element = driver.find_element_by_id('downloadButton')
+# my_element = driver.find_element_by_name('downloadButton')
+my_element = driver.find_element(By.ID, 'downloadButton')
+time.sleep(2)
 my_element.click()
-
+time.sleep(5)
